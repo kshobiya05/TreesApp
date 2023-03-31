3c2c37ecc505bd8597a60c04906ecd673face2b4
kshobiya05 <126243038+kshobiya05@users.noreply.github.com>
2023-03-31 14:50:23 +0200

    Delete data1 directory

--- data1/src/main/java/com/example/data/data/remote/dto/FacetGroup.kt
+++
@@ -1,6 +0,0 @@
-package com.example.data.data.remote.dto
-
-data class FacetGroup(
-    val facets: List<Facet>,
-    val name: String
-)
--- data1/src/main/java/com/example/data/data/remote/dto/Geometry.kt
+++
@@ -1,6 +0,0 @@
-package com.example.data.data.remote.dto
-
-data class Geometry(
-    val coordinates: List<Double>,
-    val type: String
-)
