c6b3d803dc0ef0cab2af2da9021cf6a8eeb7502b
kshobiya05 <126243038+kshobiya05@users.noreply.github.com>
2023-03-31 14:49:17 +0200

    Delete data1 directory

--- data1/src/main/java/com/example/data/data/remote/dto/TreeFullInfo.kt
+++
@@ -1,9 +0,0 @@
-package com.example.data.data.remote.dto
-
-data class TreeFullInfo(
-    val facet_groups: List<FacetGroup>,
-    val nhits: Int,
-    val parameters: Parameters,
-    val records: List<Record>
-)
-
